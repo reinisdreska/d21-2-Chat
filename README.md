f5068a35336fb2d7656610c278908bfb046f0401
[Students] Reinis Dreska <81DRDreska@rvt.lv>
2020-02-11 12:25:40 +0200

    cahts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chats
+
+Projekta piemērs.
--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from chat import Chat, render_template
+from flask import Flask, json, jsonify, render_template, request
 
-app = Chat(__name__)
+app = Flask('app')
 
-@app.route("/")
-@app.route("/index")
-def index():
-    return render_template('īndex.html')
 
-if __name__ == 'main':
-    app.run(host='0.0.0.0', threaded=True, port='5000', debug=True)
+@app.route('/')
+def index_lapa():
+  return render_template('chatbox.html')
+
+
+@app.route('/health')
+def health_check():
+  return "OK"
+
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open("chats.txt", "r", encoding="UTF-8") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({"chats": chata_rindas})
+
+
+@app.route('/chats/suuti', methods=['POST'])
+def suutiit_zinju():
+  dati = request.json
+  
+  with open("chats.txt", "a", newline="", encoding="UTF-8") as f:
+    f.write(dati["chats"] + "\n")
+
+  return ielasit_chatu()
+  
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
